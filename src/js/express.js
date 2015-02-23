af256b28da0e44fcd9570d0080c85f9395cba905
Tony <tony@raisingthefloor.org>
2015-02-23 11:51:43 +0100

    GPII-1099: Refactored to enable clean nesting of routers while preserving middleware isolation, per feedback from Antranig on the mailing list.

--- src/js/express.js
+++ src/js/express.js
@@ -6,12 +6,72 @@ var fluid = fluid || require("infusion");
 var gpii  = fluid.registerNamespace("gpii");
 fluid.registerNamespace("gpii.express");
 
-gpii.express.registerRouter = function(routerComponent, expressComponent) {
-    expressComponent.options.routersToLoad.push(routerComponent.options.router);
+// Look up the path (parent, sibling, child) relationships for a node.
+// Used in constructing the hierarchy we will use to wire together everything in our onCreate method.
+//
+// This does not require an invoker as it is basically a static method.
+gpii.express.pathForComponent = function (that) {
+    var instantiator  = fluid.getInstantiator(that);
+    var path          = instantiator.idToPath(that.id);
+    var segments      = fluid.model.parseEL(path);
+    return segments;
 };
 
-gpii.express.registerMiddleware = function(childComponent, expressComponent) {
-    expressComponent.options.middlewareToLoad.push(childComponent.getMiddlewareFunction());
+// Make a note of the lineage of this middleware or router component, all the way back presumably to this gpii.express
+// instance itself.
+//
+// This information will be used to wire together all nested routers and any middleware dependencies they have.
+gpii.express.registerComponentLineage = function(childComponent, expressComponent) {
+    // Get the list of parents for this component
+    var segments = gpii.express.pathForComponent(childComponent);
+
+    var childNickname  = segments[segments.length - 1];
+
+    // If we only have one segment, we are a child of express itself.
+    if (segments.length === 1) {
+        expressComponent.options.members.directChildrenOfInterest.push(childNickname);
+    }
+    // Otherwise, register the nickname of this component and its immediate parent path, each level will take care of the next highest.
+    else if (segments.length > 1) {
+        var parentPath = segments.slice(0, segments.length - 1).join(".");
+        if (expressComponent.options.members.childrenByParent[parentPath]) {
+            expressComponent.options.members.childrenByParent[parentPath].push(childNickname);
+        }
+        else {
+            expressComponent.options.members.childrenByParent[parentPath] = [childNickname];
+        }
+    }
+};
+
+// Wire a child to its immediate descendents.
+gpii.express.connectDirectDescendants = function(that, childComponent, childPath) {
+    var descendants =  that.options.members.childrenByParent[childPath];
+
+    // This component has descendants, wire them in first
+    if (descendants !== undefined) {
+        // Wire together one level of routers and middleware, then recurse
+        for (var a = 0; a < descendants.length; a++) {
+            var grandchildNickname  = descendants[a];
+            var grandchildComponent = childComponent[grandchildNickname];
+            if (fluid.hasGrade(grandchildComponent.options, "gpii.express.router")) {
+                childComponent.options.router.use(grandchildComponent.options.path, grandchildComponent.options.router);
+
+                // Recurse from here on down.
+                that.connectDirectDescendants(grandchildComponent, childPath + "." + grandchildNickname);
+            }
+            else if (fluid.hasGrade(grandchildComponent.options, "gpii.express.middleware")) {
+                childComponent.options.router.use(grandchildComponent.getMiddlewareFunction());
+            }
+
+        }
+    }
+
+    // After our child components are in place, wire the router to itself.
+    //
+    // This must be done here because we want to give children the chance to own part of the path before we take the rest over.
+    if (fluid.hasGrade(childComponent.options, "gpii.express.router")) {
+        childComponent.options.router.use("/", childComponent.getRouterFunction());
+    }
 };
 
 gpii.express.init = function(that) {
@@ -23,13 +83,20 @@ gpii.express.init = function(that) {
     var express  = require("express");
     that.express = express();
 
-    for (var a = 0; a < that.options.middlewareToLoad.length; a++){
-        that.express.use(that.options.middlewareToLoad[a]);
-    }
+    // TODO: We may need to reverse the wiring order to avoid clobbering sub-modules
+    // Wire together all routers and components, beginning with ourselves
+    for (var a = 0; a < that.options.members.directChildrenOfInterest.length; a++) {
+        var directChildNickname = that.options.members.directChildrenOfInterest[a];
+        var childComponent      = that[directChildNickname];
+        if (fluid.hasGrade(childComponent.options, "gpii.express.router")) {
+            that.express.use(childComponent.options.path, childComponent.options.router);
+        }
+        else if (fluid.hasGrade(childComponent.options, "gpii.express.middleware")) {
+            that.express.use(childComponent.getMiddlewareFunction());
+        }
 
-    for (var b = 0; b < that.options.routersToLoad.length; b++) {
-        // router modules set their own paths internally, so we can mount them on the root safely
-        that.express.use("/", that.options.routersToLoad[b]);
+        // Recurse from here on down.
+        that.connectDirectDescendants(childComponent, directChildNickname);
     }
 
     that.express.set("port", that.options.config.express.port);
@@ -50,26 +117,34 @@ gpii.express.stopServer = function(that) {
 
 fluid.defaults("gpii.express", {
     gradeNames: ["fluid.standardRelayComponent", "autoInit"],
-    middlewareToLoad: [],
-    routersToLoad:    [],
+    members: {
+        directChildrenOfInterest: [],
+        childrenByParent:         {}
+    },
     path: "/",
     express: null,
     distributeOptions:[
         {
             record: {
-                "funcName": "gpii.express.registerRouter",
+                "funcName": "gpii.express.registerComponentLineage",
                 "args": ["{arguments}.0", "{gpii.express}"]
             },
-            target: "{that > gpii.express.router}.options.listeners.routerLoaded"
+            target: "{that gpii.express.router}.options.listeners.routerLoaded"
         },
         {
             record: {
-                "funcName": "gpii.express.registerMiddleware",
+                "funcName": "gpii.express.registerComponentLineage",
                 "args": ["{arguments}.0", "{gpii.express}"]
             },
-            target: "{that > gpii.express.middleware}.options.listeners.onCreate"
+            target: "{that gpii.express.middleware}.options.listeners.onCreate"
         }
     ],
+    invokers: {
+        "connectDirectDescendants" : {
+            "funcName": "gpii.express.connectDirectDescendants",
+            "args":     ["{that}", "{arguments}.0", "{arguments}.1"]
+        }
+    },
     events: {
         started:  null,
         stopped:  null
--- src/js/router.js
+++ src/js/router.js
@@ -10,7 +10,7 @@ var fluid = fluid || require('infusion');
 var gpii  = fluid.registerNamespace("gpii");
 fluid.registerNamespace("gpii.express.router");
 
-// Instantiate our router, which contains the path handling function we ourselves provide as well as those of any child router components we have.
+// Instantiate our router object.  The root gpii.express object will wire everything together
 gpii.express.router.createRouter = function(that) {
     if (!that.options.config || !that.options.config.express) {
         console.error("Can't instantiate router without a working config object.")
@@ -19,45 +19,7 @@ gpii.express.router.createRouter = function(that) {
 
     var express         = require("express");
     that.options.router = express.Router();
-
-    //for (var a = 0; a < that.options.middlewareToLoad.length; a++){
-    //    that.options.router.use(that.options.middlewareToLoad[a]);
-    //}
-
-    // TODO:  Remove this once we have a working approach to avoid crawling through our components
-    if (that.options.components) {
-        var keys = Object.keys(that.options.components);
-        for (var z = 0; z < keys.length; z++) {
-            var component = that[keys[z]];
-            if (fluid.hasGrade(component.options, "gpii.express.router")) {
-                that.options.router.use(that.options.path, component.options.router);
-            }
-            else if (fluid.hasGrade(component.options, "gpii.express.router")) {
-                that.options.router.use(component.getMiddlewareFunction());
-            }
-        }
-    }
-
-    //for (var b = 0; b < that.options.routersToLoad.length; b++) {
-    //    // router modules set their own paths internally, so we can mount them on the root safely
-    //    that.options.router.use(that.options.path, that.options.routersToLoad[b]);
-    //}
-
-    // Load ourselves last to avoid clobbering our children
-    that.options.router.use(that.options.path, that.getRouterFunction());
-
     that.events.routerLoaded.fire(that);
-};
-
-// We will likely not have our routing infrastructure in place when our subcomponents are created, so we store them until we're ready.
-gpii.express.router.registerRouter = function(childRouterComponent, routerComponent) {
-    routerComponent.options.routersToLoad.push(childRouterComponent.options.router);
-};
-
-
-// We will likely not have our routing infrastructure in place when our subcomponents are created, so we store them until we're ready.
-gpii.express.router.registerMiddleware = function(childMiddlewareComponent, parentRouterComponent) {
-    parentRouterComponent.options.middlewareToLoad.push(childMiddlewareComponent.getMiddlewareFunction());
 };
 
 // If a working getRouterFunction() is not found, someone has not properly implemented their grade.
@@ -71,24 +33,6 @@ fluid.defaults("gpii.express.router", {
     middlewareToLoad: [],
     routersToLoad:    [],
     config: "{gpii.express}.options.config",
-    distributeOptions:[
-        // TODO:  Talk this through with Antranig and come up with a better solution
-        //
-        //{
-        //    source: {
-        //        "funcName": "gpii.express.router.registerRouter",
-        //        "args": ["{arguments}.0", "{that}"]
-        //    },
-        //    target: "{gpii.express.router}.options.routersToLoad"
-        //},
-        {
-            record: {
-                "funcName": "gpii.express.router.registerMiddleware",
-                "args": ["{arguments}.0", "{gpii.express.router}"]
-            },
-            target: "{that > gpii.express.middleware}.options.listeners.onCreate"
-        }
-    ],
     router: null,
     events: {
         addRoutes:       null,
