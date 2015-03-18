d0e9812d650e76a43c6af1b116a28a81b9cccb6e
Tony <tony@raisingthefloor.org>
2015-03-18 11:11:01 +0100

    Removed previous `model` cruft and simplified the `gradeNames` as well.

--- src/js/json.js
+++ src/js/json.js
@@ -4,17 +4,14 @@ var fluid  = fluid || require("infusion");
 var gpii   = fluid.registerNamespace("gpii");
 fluid.registerNamespace("gpii.express.middleware.bodyparser.json");
 
-var bp         = require("body-parser");
+var bp = require("body-parser");
 
 gpii.express.middleware.bodyparser.json.getMiddleware = function () {
     return bp.json();
 };
 
 fluid.defaults("gpii.express.middleware.bodyparser.json", {
-    gradeNames: ["fluid.standardRelayComponent", "gpii.express.middleware", "autoInit"],
-    model: {
-        middleware: ["urlencoded", "json"]
-    },
+    gradeNames: ["gpii.express.middleware", "autoInit"],
     invokers: {
         "getMiddleware": {
             funcName: "gpii.express.middleware.bodyparser.json.getMiddleware",
