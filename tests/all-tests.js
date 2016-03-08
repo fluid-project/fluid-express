1e10d93c11db5327ba12573203347ca2a5db3c5b
Tony Atkins <tony@raisingthefloor.org>
2016-03-08 12:51:12 +0100

    GPII-1647:  Added `headerSetter` middleware tests to `all-tests.js`.

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -3,6 +3,7 @@ require("./js/handler/handler-tests");
 require("./js/helpers/helpers-tests");
 require("./js/method/method-isolation-tests");
 require("./js/middleware/middleware-tests");
+require("./js/headerMiddleware/headerMiddleware-tests");
 require("./js/requestAware/requestAware-tests");
 require("./js/router/router-tests");
 require("./js/router/static/static-tests");
