6495e3a1590870f63ace28bdc2cb8f09845e6196
Tony Atkins <tony@raisingthefloor.org>
2016-05-23 16:39:28 +0200

    GPII-1800:  Disentangled node-specific and browser-safe parts of encoding/decoding for wider reuse.

--- mainIncludes.js
+++ mainIncludes.js
@@ -1,6 +1,7 @@
 "use strict";
 
 require("./src/js/lib/querystring-coding");
+require("./src/js/lib/querystring-coding-middleware");
 require("./src/js/routable");
 require("./src/js/express");
 require("./src/js/middleware");
--- src/js/lib/querystring-coding.js
+++ src/js/lib/querystring-coding.js
@@ -7,10 +7,8 @@
 
  */
 "use strict";
-var fluid = require("infusion");
+var fluid = fluid || require("infusion");
 var gpii  = fluid.registerNamespace("gpii");
-
-var url = require("url");
 
 fluid.registerNamespace("gpii.express.querystring");
 
@@ -78,43 +76,6 @@ gpii.express.querystring.decode = function (stringToDecode) {
 
     return decodedObject;
 };
-
-// Middleware to replace the built-in query parser
-fluid.registerNamespace("gpii.express.middleware.withJsonQueryParser");
-
-gpii.express.middleware.withJsonQueryParser.middleware = function (request, next) {
-    var urlObject = url.parse(request.url);
-    request.query = gpii.express.querystring.decode(urlObject.search ? urlObject.search.substring(1) : "");
-    next();
-};
-
-fluid.defaults("gpii.express.middleware.withJsonQueryParser", {
-    gradeNames: ["gpii.express.middleware"],
-    namespace: "jsonQueryParser",
-    invokers: {
-        middleware: {
-            funcName: "gpii.express.middleware.withJsonQueryParser.middleware",
-            args:     ["{arguments}.0", "{arguments}.2"] // request, response (not used), next
-        }
-    }
-});
-
-// Integrate our parser with `gpii.express`
-fluid.defaults("gpii.express.withJsonQueryParser", {
-    gradeNames: ["gpii.express"],
-    expressAppOptions: {
-        "query parser": false
-    },
-    components: {
-        queryParser: {
-            type: "gpii.express.middleware.withJsonQueryParser",
-            options: {
-                priority: "first"
-            }
-        }
-    }
-});
-
 
 fluid.registerNamespace("gpii.express.dataSource.urlEncodedJson");
 
