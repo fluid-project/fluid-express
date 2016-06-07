217cb2154ed82ba4d5a309510e338347693d0846
Tony Atkins <tony@raisingthefloor.org>
2016-06-07 08:01:22 -0400

    GPII-1829: Removed unused Grunt configuration options.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,22 +5,13 @@ module.exports = function (grunt) {
         eslint: {
             src: ["./src/**/*.js", "./tests/**/*.js", "./*.js"]
         },
-        jshint: {
-            src: ["./src/**/*.js", "./tests/**/*.js", "./*.js"],
-            buildScripts: ["Gruntfile.js"],
-            options: {
-                jshintrc: true
-            }
-        },
         jsonlint: {
             src: ["src/**/*.json", "tests/**/*.json", "./*.json"]
         }
     });
 
     grunt.loadNpmTasks("grunt-jsonlint");
-    grunt.loadNpmTasks("grunt-shell");
 
-    // grunt.loadNpmTasks("gruntify-eslint");
     grunt.loadNpmTasks("grunt-eslint");
 
     grunt.registerTask("lint", "Apply jshint and jsonlint", ["eslint", "jsonlint"]);
