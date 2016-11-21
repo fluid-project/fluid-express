4d444de39c730a756a0beb789fd40d2db8cea238
Tony Atkins <tony@raisingthefloor.org>
2016-11-21 10:03:51 +0100

    GPII-2149: Removed outdated “deduping” logic to avoid problems with recursive resolution on Windows.

--- index.js
+++ index.js
@@ -10,53 +10,14 @@
 var fluid = require("infusion");
 var gpii  = fluid.registerNamespace("gpii");
 
-var path = require("path");
-var resolve = require("resolve");
+fluid.registerNamespace("gpii.express");
+fluid.module.register("gpii-express", __dirname, require);
 
-/*
-    Assuming we are starting in `sub-module/node_modules/gpii-express`, check for the existence of `gpii-express`
-    in the directory that contains `sub-module`, i.e. `../../..`.
+require("./mainIncludes.js");
 
-    Adapted from the new "dedupe" handling in infusion:
+// Provide a function to optionally load test support.
+gpii.express.loadTestingSupport = function () {
+    require("./testIncludes.js");
+};
 
-    https://github.com/fluid-project/infusion/blob/master/src/module/fluid.js
- */
-
-var parentExpress;
-try {
-    var upPath = path.resolve(__dirname, "../../..");
-    var parentExpressPath = resolve.sync("gpii-express", {
-        basedir: upPath
-    });
-    // Per discussions with Antranig, we agreed this is not practical to test.  Exclude it from the test coverage reports.
-    /* istanbul ignore next */
-    parentExpress = require(parentExpressPath);
-} catch (e) {
-    // There will always be an exception if we are at the top level.  We choose to ignore it.
-}
-
-// Per discussions with Antranig, we agreed this is not practical to test.  Exclude it from the test coverage reports.
-/* istanbul ignore if */
-if (parentExpress) {
-    fluid.log("Resolved gpii-express from path " + __dirname + " to " + parentExpress.baseDir);
-    module.exports = parentExpress;
-    return;
-}
-else {
-    fluid.registerNamespace("gpii.express");
-    fluid.module.register("gpii-express", __dirname, require);
-
-    require("./mainIncludes.js");
-
-    // Provide a function to optionally load test support.
-    gpii.express.loadTestingSupport = function () {
-        require("./testIncludes.js");
-    };
-
-    gpii.express.baseDir = __dirname;
-
-    module.exports = gpii.express;
-
-    fluid.log("Express at path " + gpii.express.baseDir + " is at top level ");
-}
-
+module.exports = gpii.express;
